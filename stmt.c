4a5de40843f4016039643280dd3e78a567877092
pedz <pedz@easesoftware.com>
1994-08-22 17:56:35 -0500

    date and time created 94/08/22 17:56:35 by pedz
    SCCS-vsn: 1.1

---
+++ stmt.c
@@ -0,0 +1,376 @@
+static char sccs_id[] = "@(#)stmt.c	1.1";
+
+/*
+ * TODO's
+ * #### switch statements
+ */
+#include <stdio.h>
+#include <stdlib.h>
+#include "map.h"
+#include "sym.h"
+#include "tree.h"
+#include "stmt.h"
+#include "dex.h"
+
+/*
+ * Each statement that can have a break (or a continue) calls
+ * push_breaks to start the new level with the statement number.  This
+ * call creates a nesting structure.  Then calls to mk_add_break_stmt
+ * create a link structure which is added to n_list and has the
+ * statement number for the goto in the link.  When link_breaks is
+ * finally called, the n_list is traversed and the proper statement
+ * number is put in.
+ *
+ * The continues create nesting levels but the goto statements are
+ * resolved at the time the mk_cont_stmt is called.  link_conts must
+ * still be called to pop the list of link structures.
+ */
+struct nesting {
+    struct nesting *n_next;
+    struct link *n_link;
+    stmt n_stmt;
+};
+struct link {
+    struct link *l_list;		/* list of stmts to fix */
+    stmt l_stmt;			/* statement to fix */
+};
+static struct nesting *breaks;
+static struct nesting *conts;
+
+struct goto_ref {			/* used for "goto dog;" */
+    struct goto_ref *gr_next;
+    stmt gr_stmt;
+    char *gr_id;
+};
+struct goto_ref *goto_refs;
+
+struct goto_def {			/* used for "dog: i++;" */
+    struct goto_def *gd_next;
+    stmt gd_stmt;
+    char *gd_id;
+};
+struct goto_def *goto_defs;
+    
+struct stmt *statements;
+static stmt current_stmt = 1;
+static stmt current_max;
+
+static stmt next_stmt(enum stmt_type t)
+{
+
+    if (!statements)
+	statements = smalloc(sizeof(struct stmt) * (current_max = 100),
+			     __FILE__, __LINE__);
+    if (current_stmt == current_max &&
+	!(statements = realloc(statements,
+			       sizeof(struct stmt) * (current_max += 100)))) {
+	fprintf(stderr, "Out of memory for realloc in next_stmt\n");
+	exit(1);
+    }
+    statements[current_stmt].stmt_type = t;
+    return current_stmt++;
+}
+
+static struct nesting *new_nesting(void)
+{
+    struct nesting *ret = new(struct nesting);
+
+    bzero(ret, sizeof(*ret));
+    return ret;
+}
+
+static struct link *new_link(void)
+{
+    struct link *ret = new(struct link);
+
+    bzero(ret, sizeof(*ret));
+    return ret;
+}
+
+expr *execute_statement(stmt s)
+{
+    struct stmt *sp;
+    expr *e;
+    all a;
+
+    if (s == NO_STMT)
+	return 0;
+
+    sp = statements + s;
+    for ( ; ; )
+	switch (sp->stmt_type) {
+	case NULL_STMT:
+	    sp++;
+	    break;
+
+	case EXPR_STMT:
+	    if (e = sp->stmt_expr)
+		(void)i_val(e);
+	    sp++;
+	    break;
+
+	case SWITCH_STMT:
+	    break;
+
+	case GOTO_STMT:
+	    sp = statements + sp->stmt_stmt;
+	    break;
+
+	case BR_TRUE:
+	    if (i_val(sp->stmt_expr))
+		sp = statements + sp->stmt_stmt;
+	    else
+		++sp;
+	    break;
+
+	case BR_FALSE:
+	    if (i_val(sp->stmt_expr))
+		++sp;
+	    else
+		sp = statements + sp->stmt_stmt;
+	    break;
+
+	case RETURN_STMT:
+	    return sp->stmt_expr;
+
+	case ALLOC_STMT:
+	    alloc_stack(sp->stmt_alloc);
+	    ++sp;
+	    break;
+
+	case PRINT_STMT:
+	    print_expression(&sp->stmt_print);
+	    ++sp;
+	    break;
+	}
+}
+
+stmt get_current_stmt(void)
+{
+    return current_stmt;
+}
+
+stmt mk_expr_stmt(expr *e)
+{
+    stmt ret = next_stmt(EXPR_STMT);
+
+    statements[ret].stmt_expr = e;
+    return ret;
+}
+
+stmt mk_switch_stmt(expr *e)
+{
+    stmt ret = next_stmt(SWITCH_STMT);
+
+    statements[ret].stmt_expr = e;
+    return ret;
+}
+
+stmt mk_goto_stmt(stmt s)
+{
+    stmt ret = next_stmt(GOTO_STMT);
+
+    statements[ret].stmt_stmt = (s == NO_STMT) ? current_stmt : s;
+    return ret;
+}
+
+stmt mk_br_true(expr *e, stmt s)
+{
+    stmt ret = next_stmt(BR_TRUE);
+    struct stmt *sp = statements + ret;
+
+    sp->stmt_expr = e;
+    sp->stmt_stmt = (s == NO_STMT) ? current_stmt : s;
+    return ret;
+}
+
+stmt mk_br_false(expr *e, stmt s)
+{
+    stmt ret = next_stmt(BR_FALSE);
+    struct stmt *sp = statements + ret;
+
+    sp->stmt_expr = e;
+    sp->stmt_stmt = (s == NO_STMT) ? current_stmt : s;
+    return ret;
+}
+
+stmt mk_return_stmt(expr *e)
+{
+    stmt ret = next_stmt(RETURN_STMT);
+
+    statements[ret].stmt_expr = e;
+    return ret;
+}
+
+void link_stmt(stmt from_stmt, stmt to_stmt)
+{
+    statements[from_stmt].stmt_stmt =
+	(to_stmt == NO_STMT) ? current_stmt : to_stmt;
+}
+
+stmt mk_add_break_stmt(void)
+{
+    stmt ret = mk_goto_stmt(NO_STMT);
+    struct link *l = new_link();
+
+    l->l_stmt = ret;
+    l->l_list = breaks->n_link;
+    breaks->n_link = l;
+    return ret;
+}
+
+stmt mk_add_cont_stmt(void)
+{
+    stmt ret = mk_goto_stmt(NO_STMT);
+    struct link *l = new_link();
+
+    l->l_stmt = ret;
+    l->l_list = conts->n_link;
+    conts->n_link = l;
+    return ret;
+}
+
+stmt mk_add_goto_stmt(char *id)
+{
+    stmt ret = mk_goto_stmt(NO_STMT);
+    struct goto_ref *ref = new(struct goto_ref);
+
+    ref->gr_next = goto_refs;
+    goto_refs = ref;
+    ref->gr_stmt = ret;
+    ref->gr_id = id;
+    return ret;
+}
+
+void mk_goto_def(stmt s, char *id)
+{
+    struct goto_def *def = new(struct goto_def);
+
+    def->gd_next = goto_defs;
+    goto_defs = def;
+    def->gd_stmt = s;
+    def->gd_id = id;
+}
+
+void resolv_gotos(void)
+{
+    struct goto_def *def;
+    struct goto_ref *ref;
+
+    /* Link up all references */
+    while (ref = goto_refs) {
+	for (def = goto_defs;
+	     def && strcmp(def->gd_id, ref->gr_id);
+	     def = def->gd_next);
+	if (def)
+	    statements[ref->gr_stmt].stmt_stmt = def->gd_stmt;
+	else
+	    fprintf(stderr, "Label %s referenced but never defined\n",
+		    ref->gr_id);
+
+	goto_refs = ref->gr_next;
+	free(ref);
+    }
+    /* Free all definitions */
+    while (def = goto_defs) {
+	goto_defs = def->gd_next;
+	free(def);
+    }
+}
+
+stmt mk_alloc_stmt(void)
+{
+    return next_stmt(ALLOC_STMT);
+}
+
+stmt mk_print_stmt(cnode *c)
+{
+    stmt ret = next_stmt(PRINT_STMT);
+
+    statements[ret].stmt_print = *c;
+    return ret;
+}
+
+void push_breaks(stmt s)
+{
+    struct nesting *n = new_nesting();
+    
+    n->n_next = breaks;
+    n->n_stmt = s;
+    breaks = n;
+}
+
+void push_conts(stmt s)
+{
+    struct nesting *n = new_nesting();
+    
+    n->n_next = conts;
+    n->n_stmt = s;
+    conts = n;
+}
+
+stmt link_breaks(void)
+{
+    struct nesting *t = breaks;
+    stmt ret = t->n_stmt;
+    struct link *l = t->n_link;
+
+    while (l) {
+	struct link *lt = l->l_list;
+
+	statements[l->l_stmt].stmt_stmt = current_stmt;
+	free(l);
+	l = lt;
+    }
+    
+    breaks = t->n_next;
+    free(t);
+    return ret;
+}
+
+stmt link_conts(void)
+{
+    struct nesting *t = conts;
+    stmt ret = t->n_stmt;
+    struct link *l = t->n_link;
+
+    while (l) {
+	struct link *lt = l->l_list;
+
+	statements[l->l_stmt].stmt_stmt = current_stmt;
+	free(l);
+	l = lt;
+    }
+    
+    conts = t->n_next;
+    free(t);
+    return ret;
+}
+
+static char *stmt_names[LAST_STMT + 1];
+
+static void dump_init(void)
+{
+    stmt_names[NULL_STMT] = "%3d: null\n";
+    stmt_names[EXPR_STMT] = "%3d: expr 0x%08x\n";
+    stmt_names[SWITCH_STMT] = "%3d: switch (0x%08x) <%3d>\n";
+    stmt_names[GOTO_STMT] = "%3d: %.0sgoto <%3d>\n";
+    stmt_names[BR_TRUE] = "%3d: if (0x%08x) <%3d>\n";
+    stmt_names[BR_FALSE] = "%3d: if NOT (0x%08x) <%3d>\n";
+    stmt_names[RETURN_STMT] = "%3d: return (0x%08x)\n";
+    stmt_names[ALLOC_STMT] = "%3d: alloc %d\n";
+    stmt_names[PRINT_STMT] = "%3d: print (0x%08x)\n";
+}
+
+void dump_stmts(void)
+{
+    int i = 1;
+    struct stmt *s = statements + i;
+    struct stmt *send = statements + current_stmt;
+
+    if (!stmt_names[0])
+	dump_init();
+
+    for ( ; s < send; ++s, ++i)
+	printf(stmt_names[s->stmt_type], i, s->stmt_expr, s->stmt_stmt);
+}
