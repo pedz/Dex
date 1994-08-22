a308d9ae9f78bfd65cd0456b3a80ca50d46895eb
pedz <pedz@easesoftware.com>
1994-08-22 17:56:33 -0500

    date and time created 94/08/22 17:56:33 by pedz
    SCCS-vsn: 1.1

---
+++ fcall.h
@@ -0,0 +1,15 @@
+
+/* @(#)fcall.h	1.1 */
+
+void alloc_stack(int n);
+signed char sc_fcall(expr *n);
+unsigned char uc_fcall(expr *n);
+int i_fcall(expr *n);
+unsigned int ui_fcall(expr *n);
+short s_fcall(expr *n);
+unsigned short us_fcall(expr *n);
+long l_fcall(expr *n);
+unsigned long ul_fcall(expr *n);
+float f_fcall(expr *n);
+double d_fcall(expr *n);
+st st_fcall(expr *n);
