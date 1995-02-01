08eba263e892ec2363d9a8c9cd9b79fe3152908a
pedz <pedz@easesoftware.com>
1995-02-01 10:36:48 -0600

    (no message)
    SCCS-vsn: 1.2

--- dex.h
+++ dex.h
@@ -1,8 +1,12 @@
 
-/* @(#)dex.h	1.1 */
+/* @(#)dex.h	1.2 */
 
 /* globals */
-char *progname;
+extern char *progname;
+extern char *dumpname;
+extern char *unixname;
+extern void *stack_top;
 void *smalloc(int size, char *file, int lineno);
 void *srealloc(void *old, int size, char *file, int lineno);
 #define new(arg) ((arg *)smalloc(sizeof(arg), __FILE__, __LINE__))
+#define A_SIZE(arg) (sizeof(arg) / sizeof((arg)[0]))
