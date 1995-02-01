f4f839348a212010f086b43fdbc4dd85c71041bf
pedz <pedz@easesoftware.com>
1995-02-01 10:57:35 -0600

    date and time created 95/02/01 10:57:35 by pedz
    SCCS-vsn: 1.1

---
+++ asgn_expr.h
@@ -0,0 +1,125 @@
+
+/* @(#)asgn_expr.h	1.1 */
+
+signed char sc_asgn(expr *n);
+unsigned char uc_asgn(expr *n);
+int i_asgn(expr *n);
+unsigned int ui_asgn(expr *n);
+short s_asgn(expr *n);
+unsigned short us_asgn(expr *n);
+long l_asgn(expr *n);
+unsigned long ul_asgn(expr *n);
+float f_asgn(expr *n);
+double d_asgn(expr *n);
+st st_asgn(expr *n);
+
+signed char sc_plusasgn(expr *n);
+unsigned char uc_plusasgn(expr *n);
+int i_plusasgn(expr *n);
+unsigned int ui_plusasgn(expr *n);
+short s_plusasgn(expr *n);
+unsigned short us_plusasgn(expr *n);
+long l_plusasgn(expr *n);
+unsigned long ul_plusasgn(expr *n);
+float f_plusasgn(expr *n);
+double d_plusasgn(expr *n);
+
+signed char sc_minusasgn(expr *n);
+unsigned char uc_minusasgn(expr *n);
+int i_minusasgn(expr *n);
+unsigned int ui_minusasgn(expr *n);
+short s_minusasgn(expr *n);
+unsigned short us_minusasgn(expr *n);
+long l_minusasgn(expr *n);
+unsigned long ul_minusasgn(expr *n);
+float f_minusasgn(expr *n);
+double d_minusasgn(expr *n);
+
+signed char sc_timesasgn(expr *n);
+unsigned char uc_timesasgn(expr *n);
+int i_timesasgn(expr *n);
+unsigned int ui_timesasgn(expr *n);
+short s_timesasgn(expr *n);
+unsigned short us_timesasgn(expr *n);
+long l_timesasgn(expr *n);
+unsigned long ul_timesasgn(expr *n);
+float f_timesasgn(expr *n);
+double d_timesasgn(expr *n);
+
+signed char sc_divasgn(expr *n);
+unsigned char uc_divasgn(expr *n);
+int i_divasgn(expr *n);
+unsigned int ui_divasgn(expr *n);
+short s_divasgn(expr *n);
+unsigned short us_divasgn(expr *n);
+long l_divasgn(expr *n);
+unsigned long ul_divasgn(expr *n);
+float f_divasgn(expr *n);
+double d_divasgn(expr *n);
+
+signed char sc_modasgn(expr *n);
+unsigned char uc_modasgn(expr *n);
+int i_modasgn(expr *n);
+unsigned int ui_modasgn(expr *n);
+short s_modasgn(expr *n);
+unsigned short us_modasgn(expr *n);
+long l_modasgn(expr *n);
+unsigned long ul_modasgn(expr *n);
+
+signed char sc_andasgn(expr *n);
+unsigned char uc_andasgn(expr *n);
+int i_andasgn(expr *n);
+unsigned int ui_andasgn(expr *n);
+short s_andasgn(expr *n);
+unsigned short us_andasgn(expr *n);
+long l_andasgn(expr *n);
+unsigned long ul_andasgn(expr *n);
+
+signed char sc_orasgn(expr *n);
+unsigned char uc_orasgn(expr *n);
+int i_orasgn(expr *n);
+unsigned int ui_orasgn(expr *n);
+short s_orasgn(expr *n);
+unsigned short us_orasgn(expr *n);
+long l_orasgn(expr *n);
+unsigned long ul_orasgn(expr *n);
+
+signed char sc_lsasgn(expr *n);
+unsigned char uc_lsasgn(expr *n);
+int i_lsasgn(expr *n);
+unsigned int ui_lsasgn(expr *n);
+short s_lsasgn(expr *n);
+unsigned short us_lsasgn(expr *n);
+long l_lsasgn(expr *n);
+unsigned long ul_lsasgn(expr *n);
+
+signed char sc_rsasgn(expr *n);
+unsigned char uc_rsasgn(expr *n);
+int i_rsasgn(expr *n);
+unsigned int ui_rsasgn(expr *n);
+short s_rsasgn(expr *n);
+unsigned short us_rsasgn(expr *n);
+long l_rsasgn(expr *n);
+unsigned long ul_rsasgn(expr *n);
+
+signed char sc_inc(expr *n);
+unsigned char uc_inc(expr *n);
+int i_inc(expr *n);
+unsigned int ui_inc(expr *n);
+short s_inc(expr *n);
+unsigned short us_inc(expr *n);
+long l_inc(expr *n);
+unsigned long ul_inc(expr *n);
+float f_inc(expr *n);
+double d_inc(expr *n);
+
+signed char sc_dec(expr *n);
+unsigned char uc_dec(expr *n);
+int i_dec(expr *n);
+unsigned int ui_dec(expr *n);
+short s_dec(expr *n);
+unsigned short us_dec(expr *n);
+long l_dec(expr *n);
+unsigned long ul_dec(expr *n);
+float f_dec(expr *n);
+double d_dec(expr *n);
