2d853d9271e71a66d93024dcae2254ac0c6fbe3e
pedz <pedz@easesoftware.com>
1995-04-25 10:09:56 -0500

    (no message)
    SCCS-vsn: 1.2

--- builtins.h
+++ builtins.h
@@ -1,8 +1,21 @@
 
-/* @(#)builtins.h	1.1 */
+/* @(#)builtins.h	1.2 */
 
+int int_regs_instr(expr *n);
+char *int_instr(expr *n);
+char *int_strcpy(expr *n);
+char *int_strncpy(expr *n);
+char *int_strcat(expr *n);
+char *int_strncat(expr *n);
+int int_strcmp(expr *n);
+int int_strncmp(expr *n);
+char *int_strchr(expr *n);
+char *int_strrchr(expr *n);
+int int_sprintf(expr *n);
 int int_printf(expr *n);
 int int_v2f(expr *n);
 int int_find(expr *n);
 int int_dis(expr *n);
 int int_load(expr *n);
+int int_purge_user_pages(expr *n);
+int int_purge_all_pages(expr *n);
