da9795b86fe25ef6e9076abe4c8d57d266fa33ac
pedz <pedz@easesoftware.com>
1997-05-13 16:02:56 -0500

    Check pointer
    SCCS-vsn: 1.3

--- dex.h
+++ dex.h
@@ -1,12 +1,14 @@
 
-/* @(#)dex.h	1.2 */
+/* @(#)dex.h	1.3 */
 
 /* globals */
 extern char *progname;
 extern char *dumpname;
 extern char *unixname;
 extern void *stack_top;
-void *smalloc(int size, char *file, int lineno);
-void *srealloc(void *old, int size, char *file, int lineno);
-#define new(arg) ((arg *)smalloc(sizeof(arg), __FILE__, __LINE__))
+void *safe_malloc(int size, char *file, int lineno);
+void *safe_realloc(void *old, int size, char *file, int lineno);
+#define smalloc(size) (safe_malloc((size), __FILE__, __LINE__))
+#define srealloc(old, size) (safe_realloc((old), (size), __FILE__, __LINE__))
+#define new(arg) ((arg *)smalloc(sizeof(arg)))
 #define A_SIZE(arg) (sizeof(arg) / sizeof((arg)[0]))
