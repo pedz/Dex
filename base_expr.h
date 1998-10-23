fd8f51b6749c5dc631684436c8e915f0403e434d
pedz <pedz@easesoftware.com>
1998-10-23 12:26:07 -0500

    (no message)
    SCCS-vsn: 1.4

--- base_expr.h
+++ base_expr.h
@@ -1,6 +1,6 @@
-/* @(#)base_expr.h	1.3 */
+/* @(#)base_expr.h	1.4 */
 
-unsigned int get_field(void *p, int offset, int size);
-void set_field(void *p, int offset, int size, unsigned int val);
+ularge_t get_field(void *p, int offset, int size);
+void set_field(void *p, int offset, int size, ularge_t val);
 void print_out(typeptr tptr, char *addr, int offset, int size, int indent,
 	       char *name);
