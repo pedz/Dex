3d5c36b7aa76d2205d20f1ea7c8f13acbf8af003
pedz <pedz@easesoftware.com>
2002-03-14 16:12:31 -0600

    (no message)
    SCCS-vsn: 1.5

--- builtins.h
+++ builtins.h
@@ -1,22 +1,30 @@
 
-/* @(#)builtins.h	1.4 */
+/* @(#)builtins.h	1.5 */
 
-char *int_instr(expr *n);
-char *int_strcat(expr *n);
-char *int_strchr(expr *n);
-char *int_strcpy(expr *n);
-char *int_strncat(expr *n);
-char *int_strncpy(expr *n);
-char *int_strrchr(expr *n);
-int int_dis(expr *n);
-int int_find(expr *n);
-int int_load(expr *n);
-int int_printf(expr *n);
-int int_purge_all_pages(expr *n);
-int int_purge_user_pages(expr *n);
-int int_regs_instr(expr *n);
+#ifndef __BUILTINS_H
+#define __BUILTINS_H
+
 int int_return_range(expr *n);
-int int_sprintf(expr *n);
+int int_regs_instr(expr *n);
+char *int_instr(expr *n);
+char *int_strcpy(expr *n);
+char *int_strncpy(expr *n);
+char *int_strcat(expr *n);
+char *int_strncat(expr *n);
 int int_strcmp(expr *n);
+int int_strlen(expr *n);
 int int_strncmp(expr *n);
-int int_v2f(expr *n);
+char *int_strchr(expr *n);
+char *int_strrchr(expr *n);
+int int_purge_user_pages(expr *n);
+int int_purge_all_pages(expr *n);
+int int_sprintf(expr *n);
+int int_printf(expr *n);
+long int_v2f(expr *n);
+long int_f2v(expr *n);
+int int_find(expr *n);
+int int_dis(expr *n);
+int int_load(expr *n);
+void builtin_init(void);
+
+#endif /* __BUILTINS_H */
