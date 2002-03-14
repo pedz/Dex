d9d7b828dc7fc7bc8679275ddb5b23d6711f7213
pedz <pedz@easesoftware.com>
2002-03-14 16:12:31 -0600

    (no message)
    SCCS-vsn: 1.6

--- dex.h
+++ dex.h
@@ -1,15 +1,21 @@
 
-/* @(#)dex.h	1.5 */
+/* @(#)dex.h	1.6 */
+
+#ifndef __DEX_H
+#define __DEX_H
 
 /* globals */
 extern char *progname;
 extern char *dumpname;
 extern char *unixname;
 extern void *stack_top;
-void *safe_malloc(int size, char *file, int lineno);
-void *safe_realloc(void *old, int size, char *file, int lineno);
+extern unsigned long debug_mask;
+
+void *safe_malloc(size_t size, char *file, int lineno);
+void *safe_realloc(void *old, size_t size, size_t old_size, char *file, int lineno);
 #define smalloc(size) (safe_malloc((size), __FILE__, __LINE__))
-#define srealloc(old, size) (safe_realloc((old), (size), __FILE__, __LINE__))
+#define srealloc(old, size, osize) \
+	(safe_realloc((old), (size),(osize),  __FILE__, __LINE__))
 #define new(arg) ((arg *)smalloc(sizeof(arg)))
 #define A_SIZE(arg) (sizeof(arg) / sizeof((arg)[0]))
 
@@ -25,3 +31,34 @@ int is_zero(char *s);
 int is_neg(char *s);
 ularge_t atoularge(char *s);
 large_t atolarge(char *);
+
+enum debug_bits {
+    BASE_C_BIT          = 0x00000001,
+    BASE_EXPR_C_BIT     = 0x00000002,
+    BINARY_EXPR_C_BIT   = 0x00000004,
+    BUILTINS_C_BIT      = 0x00000008,
+    CAST_EXPR_C_BIT     = 0x00000010,
+    DEX_C_BIT           = 0x00000020,
+    DISASM_C_BIT        = 0x00000040,
+    FCALL_C_BIT         = 0x00000080,
+    GRAM_Y_BIT          = 0x00000100,
+    LOAD_C_BIT          = 0x00000200,
+    MAP_C_BIT           = 0x00000400,
+    PRE_ASGN_EXPR_C_BIT = 0x00000800,
+    PRINT_C_BIT         = 0x00001000,
+    SCAN_L_BIT          = 0x00002000,
+    STAB_Y_BIT          = 0x00004000,
+    STMT_C_BIT          = 0x00008000,
+    SYM_C_BIT           = 0x00010000,
+    TREE_C_BIT          = 0x00020000,
+    TREE_DUMP_C_BIT     = 0x00040000,
+    UNARY_EXPR_C_BIT    = 0x00080000,
+    QUICK_RMAP_BIT      = 0x00100000
+};
+
+char *print_field(int size, ...);
+
+#define P(f) print_field(sizeof(f), (f))
+#define DEBUG_PRINTF(b) ((debug_mask & DEBUG_BIT) && printf b)
+
+#endif /* __DEX_H */
