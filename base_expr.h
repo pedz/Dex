e462331ec1d166ca6671ef3f152367464f603524
pedz <pedz@easesoftware.com>
2002-03-14 16:12:30 -0600

    (no message)
    SCCS-vsn: 1.6

--- base_expr.h
+++ base_expr.h
@@ -1,6 +1,10 @@
-/* @(#)base_expr.h	1.5 */
+
+/* @(#)base_expr.h	1.6 */
+
+#ifndef __BASE_EXPR_H
+#define __BASE_EXPR_H
 
 ularge_t get_field(void *p, int offset, int size);
 void set_field(void *p, int offset, int size, ularge_t val);
-void print_out(typeptr tptr, char *addr, int offset, int size, int indent,
-	       char *name);
+
+#endif /* __BASE_EXPR_H */
