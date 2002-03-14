9a18e6a3c96ca9280ea5e3b44e5e72bcbdf61007
pedz <pedz@easesoftware.com>
2002-03-14 16:12:32 -0600

    (no message)
    SCCS-vsn: 1.3

--- disasm.h
+++ disasm.h
@@ -1,6 +1,11 @@
 
-/* @(#)disasm.h	1.2 */
+/* @(#)disasm.h	1.3 */
+
+#ifndef __DISASM_H
+#define __DISASM_H
 
 char *instr(long *addr);
 void dis(long *addr, int count);
 int regs_instr(long *addr, int count);
+
+#endif /* __DISASM_H */
