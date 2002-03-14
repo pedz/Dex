8c0fb442d51144d2bb24ea5612718612e42e69c1
pedz <pedz@easesoftware.com>
2002-03-14 16:12:35 -0600

    (no message)
    SCCS-vsn: 1.6

--- unary_expr.c
+++ unary_expr.c
@@ -1,4 +1,4 @@
-static char sccs_id[] = "@(#)unary_expr.c	1.5";
+static char sccs_id[] = "@(#)unary_expr.c	1.6";
 
 #include <stdio.h>
 #include "dex.h"
@@ -8,10 +8,11 @@ static char sccs_id[] = "@(#)unary_expr.c	1.5";
 #include "unary_expr.h"
 #include "base_expr.h"
 #include "fcall.h"
+#define DEBUG_BIT UNARY_EXPR_C_BIT
 
 /* simple constant leaf nodes */
 
-signed char sc_leaf(expr *n) { return n->e_c; }
+signed char sc_leaf(expr *n) { return n->e_sc; }
 unsigned char uc_leaf(expr *n) { return n->e_uc; }
 int i_leaf(expr *n) { return n->e_i; }
 unsigned int ui_leaf(expr *n) { return n->e_ui; }
@@ -19,6 +20,8 @@ short s_leaf(expr *n) { return n->e_s; }
 unsigned short us_leaf(expr *n) { return n->e_us; }
 long l_leaf(expr *n) { return n->e_l; }
 unsigned long ul_leaf(expr *n) { return n->e_ul; }
+long long ll_leaf(expr *n) { return n->e_ll; }
+unsigned long long ull_leaf(expr *n) { return n->e_ull; }
 float f_leaf(expr *n) { return n->e_f; }
 double d_leaf(expr *n) { return n->e_d; }
 st st_leaf(expr *n) { return n->e_st; }
@@ -62,6 +65,16 @@ long *l_gaddr(expr *n)
 unsigned long *ul_gaddr(expr *n)
 {
     return (unsigned long *)n->e_addr;
+}
+
+long long *ll_gaddr(expr *n)
+{
+    return (long long *)n->e_addr;
+}
+
+unsigned long long *ull_gaddr(expr *n)
+{
+    return (unsigned long long *)n->e_addr;
 }
 
 float *f_gaddr(expr *n)
@@ -121,6 +134,16 @@ unsigned long *ul_laddr(expr *n)
     return (unsigned long *)(n->e_addr + frame_ptr);
 }
 
+long long *ll_laddr(expr *n)
+{
+    return (long long *)(n->e_addr + frame_ptr);
+}
+
+unsigned long long *ull_laddr(expr *n)
+{
+    return (unsigned long long *)(n->e_addr + frame_ptr);
+}
+
 float *f_laddr(expr *n)
 {
     return (float *)(n->e_addr + frame_ptr);
@@ -140,8 +163,8 @@ st *st_laddr(expr *n)
 signed char sc_l2p(expr *n)
 {
     if (n->e_bsize)
-	return (signed char)get_field(v2f(sc_addr(n->e_left)),
-			 n->e_boffset, n->e_bsize);
+	return (signed char)get_field(V2F(sc_addr(n->e_left)),
+				      n->e_boffset, n->e_bsize);
     else
 	return *v2f_type(signed char *, sc_addr(n->e_left));
 }
@@ -149,8 +172,8 @@ signed char sc_l2p(expr *n)
 unsigned char uc_l2p(expr *n)
 {
     if (n->e_bsize)
-	return (unsigned char)get_field(v2f(uc_addr(n->e_left)),
-			 n->e_boffset, n->e_bsize);
+	return (unsigned char)get_field(V2F(uc_addr(n->e_left)),
+					n->e_boffset, n->e_bsize);
     else
 	return *v2f_type(unsigned char *, uc_addr(n->e_left));
 }
@@ -158,8 +181,8 @@ unsigned char uc_l2p(expr *n)
 int i_l2p(expr *n)
 {
     if (n->e_bsize)
-	return (int)get_field(v2f(i_addr(n->e_left)),
-			 n->e_boffset, n->e_bsize);
+	return (int)get_field(V2F(i_addr(n->e_left)),
+			      n->e_boffset, n->e_bsize);
     else
 	return *v2f_type(int *, i_addr(n->e_left));
 }
@@ -167,8 +190,8 @@ int i_l2p(expr *n)
 unsigned int ui_l2p(expr *n)
 {
     if (n->e_bsize)
-	return (unsigned int)get_field(v2f(ui_addr(n->e_left)),
-			 n->e_boffset, n->e_bsize);
+	return (unsigned int)get_field(V2F(ui_addr(n->e_left)),
+				       n->e_boffset, n->e_bsize);
     else
 	return *v2f_type(unsigned int *, ui_addr(n->e_left));
 }
@@ -176,8 +199,8 @@ unsigned int ui_l2p(expr *n)
 short s_l2p(expr *n)
 {
     if (n->e_bsize)
-	return (short)get_field(v2f(s_addr(n->e_left)),
-			 n->e_boffset, n->e_bsize);
+	return (short)get_field(V2F(s_addr(n->e_left)),
+				n->e_boffset, n->e_bsize);
     else
 	return *v2f_type(short *, s_addr(n->e_left));
 }
@@ -185,8 +208,8 @@ short s_l2p(expr *n)
 unsigned short us_l2p(expr *n)
 {
     if (n->e_bsize)
-	return (unsigned short)get_field(v2f(us_addr(n->e_left)),
-			 n->e_boffset, n->e_bsize);
+	return (unsigned short)get_field(V2F(us_addr(n->e_left)),
+					 n->e_boffset, n->e_bsize);
     else
 	return *v2f_type(unsigned short *, us_addr(n->e_left));
 }
@@ -194,8 +217,8 @@ unsigned short us_l2p(expr *n)
 long l_l2p(expr *n)
 {
     if (n->e_bsize)
-	return (long)get_field(v2f(l_addr(n->e_left)),
-			 n->e_boffset, n->e_bsize);
+	return (long)get_field(V2F(l_addr(n->e_left)),
+			       n->e_boffset, n->e_bsize);
     else if (n->e_size == sizeof(ularge_t))
 	return *v2f_type(large_t *, ul_addr(n->e_left));
     else
@@ -205,13 +228,33 @@ long l_l2p(expr *n)
 unsigned long ul_l2p(expr *n)
 {
     if (n->e_bsize)
-	return (unsigned long)get_field(v2f(ul_addr(n->e_left)),
+	return (unsigned long)get_field(V2F(ul_addr(n->e_left)),
 					n->e_boffset, n->e_bsize);
     if (n->e_size == sizeof(ularge_t)) {
-	PRINTF("hey %08x\n", n);
+	DEBUG_PRINTF(("hey %s\n", P(n)));
 	return *v2f_type(ularge_t *, ul_addr(n->e_left));
     }
     return *v2f_type(unsigned long *, ul_addr(n->e_left));
+}
+
+long long ll_l2p(expr *n)
+{
+    if (n->e_bsize)
+	return (long)get_field(V2F(ll_addr(n->e_left)),
+			       n->e_boffset, n->e_bsize);
+    return *v2f_type(long *, ll_addr(n->e_left));
+}
+
+unsigned long long ull_l2p(expr *n)
+{
+    if (n->e_bsize)
+	return (unsigned long)get_field(V2F(ull_addr(n->e_left)),
+					n->e_boffset, n->e_bsize);
+    if (n->e_size == sizeof(ularge_t)) {
+	DEBUG_PRINTF(("hey %s\n", P(n)));
+	return *v2f_type(ularge_t *, ull_addr(n->e_left));
+    }
+    return *v2f_type(unsigned long *, ull_addr(n->e_left));
 }
 
 float f_l2p(expr *n)
@@ -238,121 +281,11 @@ short s_null(expr *n) { return 0; }
 unsigned short us_null(expr *n) { return 0; }
 long l_null(expr *n) { return 0; }
 unsigned long ul_null(expr *n) { return 0; }
+long long ll_null(expr *n) { return 0; }
+unsigned long long ull_null(expr *n) { return 0; }
 float f_null(expr *n) { return 0; }
 double d_null(expr *n) { return 0; }
 st st_null(expr *n) { static st s; return s; }
-
-#ifdef Not_Used
-signed char *sc_v2f(expr *n)
-{
-    return v2f_type(signed char *, sc_addr(n->e_left));
-}
-
-unsigned char *uc_v2f(expr *n)
-{
-    return v2f_type(unsigned char *, uc_addr(n->e_left));
-}
-
-int *i_v2f(expr *n)
-{
-    return v2f_type(int *, i_addr(n->e_left));
-}
-
-unsigned int *ui_v2f(expr *n)
-{
-    return v2f_type(unsigned int *, ui_addr(n->e_left));
-}
-
-short *s_v2f(expr *n)
-{
-    return v2f_type(short *, s_addr(n->e_left));
-}
-
-unsigned short *us_v2f(expr *n)
-{
-    return v2f_type(unsigned short *, us_addr(n->e_left));
-}
-
-long *l_v2f(expr *n)
-{
-    return v2f_type(long *, l_addr(n->e_left));
-}
-
-unsigned long *ul_v2f(expr *n)
-{
-    return v2f_type(unsigned long *, ul_addr(n->e_left));
-}
-
-float *f_v2f(expr *n)
-{
-    return v2f_type(float *, f_addr(n->e_left));
-}
-
-double *d_v2f(expr *n)
-{
-    return v2f_type(double *, d_addr(n->e_left));
-}
-
-st *st_v2f(expr *n)
-{
-    return v2f_type(st *, st_addr(n->e_left));
-}
-
-signed char *sc_f2v(expr *n)
-{
-    return f2v_type(signed char *, sc_addr(n->e_left));
-}
-
-unsigned char *uc_f2v(expr *n)
-{
-    return f2v_type(unsigned char *, uc_addr(n->e_left));
-}
-
-int *i_f2v(expr *n)
-{
-    return f2v_type(int *, i_addr(n->e_left));
-}
-
-unsigned int *ui_f2v(expr *n)
-{
-    return f2v_type(unsigned int *, ui_addr(n->e_left));
-}
-
-short *s_f2v(expr *n)
-{
-    return f2v_type(short *, s_addr(n->e_left));
-}
-
-unsigned short *us_f2v(expr *n)
-{
-    return f2v_type(unsigned short *, us_addr(n->e_left));
-}
-
-long *l_f2v(expr *n)
-{
-    return f2v_type(long *, l_addr(n->e_left));
-}
-
-unsigned long *ul_f2v(expr *n)
-{
-    return f2v_type(unsigned long *, ul_addr(n->e_left));
-}
-
-float *f_f2v(expr *n)
-{
-    return f2v_type(float *, f_addr(n->e_left));
-}
-
-double *d_f2v(expr *n)
-{
-    return f2v_type(double *, d_addr(n->e_left));
-}
-
-st *st_f2v(expr *n)
-{
-    return f2v_type(st *, st_addr(n->e_left));
-}
-#endif
 
 int sc_lnot(expr *n) { return !sc_val(n->e_left); }
 signed char sc_bnot(expr *n) { return ~sc_val(n->e_left); }
@@ -378,6 +311,12 @@ long l_umin(expr *n) { return -l_val(n->e_left); }
 int ul_lnot(expr *n) { return !ul_val(n->e_left); }
 unsigned long ul_bnot(expr *n) { return ~ul_val(n->e_left); }
 unsigned long ul_umin(expr *n) { return -ul_val(n->e_left); }
+int ll_lnot(expr *n) { return !ll_val(n->e_left); }
+long long ll_bnot(expr *n) { return ~ll_val(n->e_left); }
+long long ll_umin(expr *n) { return -ll_val(n->e_left); }
+int ull_lnot(expr *n) { return !ull_val(n->e_left); }
+unsigned long long ull_bnot(expr *n) { return ~ull_val(n->e_left); }
+unsigned long long ull_umin(expr *n) { return -ull_val(n->e_left); }
 int f_lnot(expr *n) { return !f_val(n->e_left); }
 float f_umin(expr *n) { return -f_val(n->e_left); }
 int d_lnot(expr *n) { return !d_val(n->e_left); }
