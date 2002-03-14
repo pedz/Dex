e641acca6481b9bee34a3a642f3788ddeb59c559
pedz <pedz@easesoftware.com>
2002-03-14 16:12:32 -0600

    (no message)
    SCCS-vsn: 1.4

--- fcall.h
+++ fcall.h
@@ -1,9 +1,13 @@
 
-/* @(#)fcall.h	1.3 */
+/* @(#)fcall.h	1.4 */
+
+#ifndef __FCALL_H
+#define __FCALL_H
 
 extern long frame_ptr;
 extern long stack_ptr;
 
+int setup_pseudos(void);
 void alloc_stack(int n);
 signed char sc_fcall(expr *n);
 unsigned char uc_fcall(expr *n);
@@ -13,6 +17,10 @@ short s_fcall(expr *n);
 unsigned short us_fcall(expr *n);
 long l_fcall(expr *n);
 unsigned long ul_fcall(expr *n);
+long long ll_fcall(expr *n);
+unsigned long long ull_fcall(expr *n);
 float f_fcall(expr *n);
 double d_fcall(expr *n);
 st st_fcall(expr *n);
+
+#endif /* __FCALL_H */
